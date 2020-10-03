6a63970ed1e4d242d0a083fce36308372fc9d232
Hasnaa Messaoudi <hasnaa.messaoudi@gmail.com>
2020-10-02 18:33:43 -0700

    Building a Hook that manage transition and back actions

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  const transition = (mode, replace = false) => {
+    if (!replace){
+      setHistory([...history, mode]);
+    }
+    setMode(mode);
+
+}
+
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+    }
+    setMode(history[history.length - 1]);
+  };
+
+  return {mode, transition, back};
+};
